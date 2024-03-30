06e430360530cb1537f5c04945b069f536f28ec8
Jhynjhiruu <cub3r12@gmail.com>
2024-03-30 01:40:58 +0000

    nand

--- src/main.c
+++ src/main.c
@@ -1,129 +1,216 @@
+#include <../../src/utils.h>
 #include <libdragon.h>
+#include <stdint.h>
 #include <stdio.h>
 
-#include <t3d/t3d.h>
-#include <t3d/t3dmath.h>
+volatile uint32_t *MI_HW_INTR_MASK = ((volatile uint32_t *)0xA430003C);
 
-#define NUM_FRAMEBUFFERS (3)
+#define PI_BB_NAND_CTRL ((volatile uint32_t *)0xA4600048)
+#define PI_BB_RD_LEN ((volatile uint32_t *)0xA4600058)
+#define PI_BB_WR_LEN ((volatile uint32_t *)0xA460005C)
+#define PI_BB_NAND_ADDR ((volatile uint32_t *)0xA4600070)
 
+#define PI_BB_BUFFER_0 ((volatile uint32_t *)0xA4610000) ///< NAND buffer 0
+#define PI_BB_BUFFER_1 ((volatile uint32_t *)0xA4610200) ///< NAND buffer 1
+#define PI_BB_SPARE_0 ((volatile uint32_t *)0xA4610400)  ///< NAND spare data 0
+#define PI_BB_SPARE_1 ((volatile uint32_t *)0xA4610410)  ///< NAND spare data 1
+#define PI_BB_AES_KEY ((volatile uint32_t *)0xA4610420)  ///< AES expanded key
+#define PI_BB_AES_IV ((volatile uint32_t *)0xA46104D0)   ///< AES initialization vector
+
+#define PI_BB_NAND_CTRL_BUSY (1 << 31)
+#define PI_BB_NAND_CTRL_ERROR (1 << 10)
+
+#define PI_BB_WNAND_CTRL_CMD_SHIFT 16
+#define PI_BB_WNAND_CTRL_LEN_SHIFT 0
+#define PI_BB_WNAND_CTRL_MULTICYCLE (1 << 10)
+#define PI_BB_WNAND_CTRL_ECC (1 << 11)
+#define PI_BB_WNAND_CTRL_BUF(n) ((n) << 14)
+#define PI_BB_WNAND_CTRL_INTERRUPT (1 << 30)
+#define PI_BB_WNAND_CTRL_EXECUTE (1 << 31)
+
+typedef enum {
+    NAND_CMD_READ1_H0 = (0x00 << PI_BB_WNAND_CTRL_CMD_SHIFT) | (1 << 28) | (1 << 27) | (1 << 26) | (1 << 25) | (1 << 24) | (1 << 15),
+    NAND_CMD_READ1_H1 = (0x01 << PI_BB_WNAND_CTRL_CMD_SHIFT) | (1 << 28) | (1 << 27) | (1 << 26) | (1 << 25) | (1 << 24) | (1 << 15),
+    NAND_CMD_RESET = (0xFF << PI_BB_WNAND_CTRL_CMD_SHIFT),
+    NAND_CMD_READID = (0x90 << PI_BB_WNAND_CTRL_CMD_SHIFT) | (1 << 28) | (1 << 24),
+    NAND_CMD_PAGEPROG_A = (0x80 << PI_BB_WNAND_CTRL_CMD_SHIFT),
+    NAND_CMD_PAGEPROG_B = (0x10 << PI_BB_WNAND_CTRL_CMD_SHIFT),
+    NAND_CMD_COPYBACK_A = (0x00 << PI_BB_WNAND_CTRL_CMD_SHIFT),
+    NAND_CMD_COPYBACK_B = (0x8A << PI_BB_WNAND_CTRL_CMD_SHIFT),
+    NAND_CMD_COPYBACK_C = (0x10 << PI_BB_WNAND_CTRL_CMD_SHIFT),
+    NAND_CMD_ERASE_A = (0x60 << PI_BB_WNAND_CTRL_CMD_SHIFT),
+    NAND_CMD_ERASE_B = (0xD0 << PI_BB_WNAND_CTRL_CMD_SHIFT),
+    NAND_CMD_READSTATUS = (0x70 << PI_BB_WNAND_CTRL_CMD_SHIFT)
+} nand_cmd_t;
+
+typedef uint32_t nand_addr_t;
+
+#define NAND_PAGE_SIZE 0x200
+#define NAND_BLOCK_SIZE 0x4000
+
+#define ADDR_OFFSET(addr) ((addr) & 0x1FF)
+#define ADDR_PAGE(addr) (((addr) >> 9) & 0x1F)
+#define ADDR_BLOCK(addr) (((addr) >> 14) & 0xFFF)
+
+static void nand_write_intbuffer(int bufidx, int offset, const void *data, int len) {
+    dma_wait();
+    *PI_DRAM_ADDR = PhysicalAddr(data);
+    *PI_CART_ADDR = offset + bufidx * 0x200;
+    *PI_BB_RD_LEN = len;
+    dma_wait();
+}
+
+static void nand_read_intbuffer(int bufidx, int offset, void *data, int len) {
+    dma_wait();
+    *PI_DRAM_ADDR = PhysicalAddr(data);
+    *PI_CART_ADDR = offset + bufidx * 0x200;
+    *PI_BB_WR_LEN = len;
+    dma_wait();
+}
+
+static void nand_cmd_wait(void) {
+    while (*PI_BB_NAND_CTRL & PI_BB_NAND_CTRL_BUSY) {
+    }
+}
+
+static void nand_cmd_readid(int bufidx) {
+    *PI_BB_NAND_ADDR = 0;
+    *PI_BB_NAND_CTRL = PI_BB_WNAND_CTRL_EXECUTE | PI_BB_WNAND_CTRL_BUF(bufidx) | NAND_CMD_READID | (4 << PI_BB_WNAND_CTRL_LEN_SHIFT);
+    nand_cmd_wait();
+}
+
+static void nand_cmd_read1(int bufidx, uint32_t addr, int len) {
+    assert(len > 0 && len <= 512);
+    *PI_BB_NAND_ADDR = addr;
+    *PI_BB_NAND_CTRL = PI_BB_WNAND_CTRL_EXECUTE | PI_BB_WNAND_CTRL_BUF(bufidx) | ((addr & 0x100) ? NAND_CMD_READ1_H1 : NAND_CMD_READ1_H0) | (len << PI_BB_WNAND_CTRL_LEN_SHIFT);
+    nand_cmd_wait();
+}
+
+void nand_read_id(uint8_t id[4]) {
+    uint8_t aligned_buf[16] __attribute__((aligned(16)));
+    data_cache_hit_invalidate(aligned_buf, 16);
+
+    const int bufidx = 0;
+    nand_cmd_readid(bufidx);
+    nand_read_intbuffer(bufidx, 0, aligned_buf, 4);
+
+    memcpy(id, aligned_buf, 4);
+}
+
+static uint8_t io_read8(uint32_t addr) {
+    uint32_t data = io_read(addr & ~3);
+    return (data >> ((~addr & 3) * 8)) & 0xFF;
+}
+
+void nand_read_data(nand_addr_t addr, uint8_t *buffer, int len) {
+    int bufidx = 0;
+
+    while (len > 0) {
+        int read_len = MIN(len, NAND_PAGE_SIZE - ADDR_OFFSET(addr));
+        nand_cmd_read1(bufidx, addr, read_len);
+        for (int i = 0; i < read_len; i++)
+            buffer[i] = io_read8((uint32_t)PI_BB_BUFFER_0 + bufidx * 0x200 + i);
+
+        addr += read_len;
+        buffer += read_len;
+        len -= read_len;
+    }
+}
 int main(void) {
-    surface_t depth_buffer;
+    uint8_t block_buf[0x4000] __attribute__((aligned(4))) = {0};
+    uint32_t *words = (void *)block_buf;
 
-    T3DMat4 model_matrix;
-    T3DMat4FP *model_matrix_fp;
+    *MI_HW_INTR_MASK = (1 << 25) | (1 << 27);
 
-    T3DVec3 cam_pos = {{0, 0, -18}};
-    float cam_rot[2] = {0.5f, 0.0f};
+    console_init();
 
-    uint8_t ambient_colour[4] = {50, 50, 50, 0xFF};
+    console_set_render_mode(RENDER_MANUAL);
 
-    uint8_t colour_dir[4] = {0xFF, 0xFF, 0xFF, 0xFF};
+    data_cache_hit_writeback(block_buf, 0x4000);
 
-    T3DVec3 light_dir = {{0.0f, 0.0f, 1.0f}};
+    nand_write_intbuffer(0, 0, block_buf, 0x200);
 
-    T3DVertPacked *vertices;
-
-    uint16_t norm;
-
-    T3DViewport viewport;
-
-    rspq_block_t *dpl_draw = NULL;
-
-    // ---------
-
-    display_init(RESOLUTION_640x480, DEPTH_16_BPP, NUM_FRAMEBUFFERS, GAMMA_NONE, FILTERS_RESAMPLE);
-
-    depth_buffer = surface_alloc(FMT_RGBA16, display_get_width(), display_get_height());
-
-    rdpq_init();
-
-    t3d_init();
-
-    t3d_mat4_identity(&model_matrix);
-
-    model_matrix_fp = malloc_uncached(sizeof(T3DMat4FP));
-
-    t3d_vec3_norm(&light_dir);
-
-    vertices = malloc_uncached(sizeof(T3DVertPacked) * 2);
-
-    norm = t3d_vert_pack_normal(&(T3DVec3){{0, 0, 1}});
-    vertices[0] = (T3DVertPacked){
-        .posA = {-16, -16, 0},
-        .rgbaA = 0xFF0000'FF,
-        .normA = norm,
-        .posB = {16, -16, 0},
-        .rgbaB = 0x00FF00'FF,
-        .normB = norm,
-    };
-    vertices[1] = (T3DVertPacked){
-        .posA = {16, 16, 0},
-        .rgbaA = 0x0000FF'FF,
-        .normA = norm,
-        .posB = {-16, 16, 0},
-        .rgbaB = 0xFF00FF'FF,
-        .normB = norm,
-    };
-
-    viewport = t3d_viewport_create();
+    nand_read_data(0, block_buf, 32);
 
     while (1) {
-        float sin, cos;
-        fm_sincosf(cam_rot[0], &sin, &cos);
+        uint32_t index = 0;
+        console_clear();
 
-        const T3DVec3 up = (T3DVec3){{0.0f, 1.0f, 0.0f}};
-        const T3DVec3 right = (T3DVec3){{-cos, 0.0f, sin}};
+        printf("Hello from libdragon in SA2 context!\n");
 
-        t3d_viewport_set_projection(&viewport, T3D_DEG_TO_RAD(85.0f), 10.0f, 100.0f);
-        /*t3d_mat4_identity(&viewport.matCamera);
-        t3d_mat4_to_fixed(&viewport._matCameraFP, &viewport.matCamera);
-        data_cache_hit_writeback(&viewport._matCameraFP, sizeof(T3DMat4FP));*/
-        // t3d_viewport_look_at(&viewport, &cam_pos, &(T3DVec3){{0, 0, 0}});
-        // t3d_mat4_from_srt_euler(&viewport.matCamera, (T3DVec3){{1, 1, 1}}.v, (T3DVec3){{0, 0.5, 0}}.v, cam_pos.v);
-        t3d_mat4_identity(&viewport.matCamera);
-        t3d_mat4_rotate(&viewport.matCamera, &up, cam_rot[0]);
-        t3d_mat4_rotate(&viewport.matCamera, &right, cam_rot[1]);
-        t3d_mat4_translate(&viewport.matCamera, cam_pos.v[0], cam_pos.v[1], cam_pos.v[2]);
-        t3d_mat4_to_fixed(&viewport._matCameraFP, &viewport.matCamera);
-        data_cache_hit_writeback(&viewport._matCameraFP, sizeof(T3DMat4FP));
+        printf("First few SK words:\n%08lX%08lX%08lX%08lX%08lX%08lX%08lX%08lX", words[index], words[index + 1], words[index + 2], words[index + 3], words[index + 4],
+               words[index + 5], words[index + 6], words[index + 7]);
+        index += 8;
+        printf("%08lX%08lX%08lX%08lX%08lX%08lX%08lX%08lX", words[index], words[index + 1], words[index + 2], words[index + 3], words[index + 4], words[index + 5], words[index + 6],
+               words[index + 7]);
+        index += 8;
+        printf("%08lX%08lX%08lX%08lX%08lX%08lX%08lX%08lX", words[index], words[index + 1], words[index + 2], words[index + 3], words[index + 4], words[index + 5], words[index + 6],
+               words[index + 7]);
+        index += 8;
+        printf("%08lX%08lX%08lX%08lX%08lX%08lX%08lX%08lX", words[index], words[index + 1], words[index + 2], words[index + 3], words[index + 4], words[index + 5], words[index + 6],
+               words[index + 7]);
+        index += 8;
+        printf("%08lX%08lX%08lX%08lX%08lX%08lX%08lX%08lX", words[index], words[index + 1], words[index + 2], words[index + 3], words[index + 4], words[index + 5], words[index + 6],
+               words[index + 7]);
+        index += 8;
+        printf("%08lX%08lX%08lX%08lX%08lX%08lX%08lX%08lX", words[index], words[index + 1], words[index + 2], words[index + 3], words[index + 4], words[index + 5], words[index + 6],
+               words[index + 7]);
+        index += 8;
+        printf("%08lX%08lX%08lX%08lX%08lX%08lX%08lX%08lX", words[index], words[index + 1], words[index + 2], words[index + 3], words[index + 4], words[index + 5], words[index + 6],
+               words[index + 7]);
+        index += 8;
+        printf("%08lX%08lX%08lX%08lX%08lX%08lX%08lX%08lX", words[index], words[index + 1], words[index + 2], words[index + 3], words[index + 4], words[index + 5], words[index + 6],
+               words[index + 7]);
+        index += 8;
+        printf("%08lX%08lX%08lX%08lX%08lX%08lX%08lX%08lX", words[index], words[index + 1], words[index + 2], words[index + 3], words[index + 4], words[index + 5], words[index + 6],
+               words[index + 7]);
+        index += 8;
+        printf("%08lX%08lX%08lX%08lX%08lX%08lX%08lX%08lX", words[index], words[index + 1], words[index + 2], words[index + 3], words[index + 4], words[index + 5], words[index + 6],
+               words[index + 7]);
+        index += 8;
+        printf("%08lX%08lX%08lX%08lX%08lX%08lX%08lX%08lX", words[index], words[index + 1], words[index + 2], words[index + 3], words[index + 4], words[index + 5], words[index + 6],
+               words[index + 7]);
+        index += 8;
+        printf("%08lX%08lX%08lX%08lX%08lX%08lX%08lX%08lX", words[index], words[index + 1], words[index + 2], words[index + 3], words[index + 4], words[index + 5], words[index + 6],
+               words[index + 7]);
+        index += 8;
+        printf("%08lX%08lX%08lX%08lX%08lX%08lX%08lX%08lX", words[index], words[index + 1], words[index + 2], words[index + 3], words[index + 4], words[index + 5], words[index + 6],
+               words[index + 7]);
+        index += 8;
+        printf("%08lX%08lX%08lX%08lX%08lX%08lX%08lX%08lX", words[index], words[index + 1], words[index + 2], words[index + 3], words[index + 4], words[index + 5], words[index + 6],
+               words[index + 7]);
+        index += 8;
+        printf("%08lX%08lX%08lX%08lX%08lX%08lX%08lX%08lX", words[index], words[index + 1], words[index + 2], words[index + 3], words[index + 4], words[index + 5], words[index + 6],
+               words[index + 7]);
+        index += 8;
+        printf("%08lX%08lX%08lX%08lX%08lX%08lX%08lX%08lX\n", words[index], words[index + 1], words[index + 2], words[index + 3], words[index + 4], words[index + 5],
+               words[index + 6], words[index + 7]);
+        index += 8;
+        printf("%08lX%08lX%08lX%08lX%08lX%08lX%08lX%08lX", words[index], words[index + 1], words[index + 2], words[index + 3], words[index + 4], words[index + 5], words[index + 6],
+               words[index + 7]);
+        index += 8;
+        printf("%08lX%08lX%08lX%08lX%08lX%08lX%08lX%08lX", words[index], words[index + 1], words[index + 2], words[index + 3], words[index + 4], words[index + 5], words[index + 6],
+               words[index + 7]);
+        index += 8;
+        printf("%08lX%08lX%08lX%08lX%08lX%08lX%08lX%08lX", words[index], words[index + 1], words[index + 2], words[index + 3], words[index + 4], words[index + 5], words[index + 6],
+               words[index + 7]);
+        index += 8;
+        printf("%08lX%08lX%08lX%08lX%08lX%08lX%08lX%08lX", words[index], words[index + 1], words[index + 2], words[index + 3], words[index + 4], words[index + 5], words[index + 6],
+               words[index + 7]);
+        index += 8;
+        printf("%08lX%08lX%08lX%08lX%08lX%08lX%08lX%08lX", words[index], words[index + 1], words[index + 2], words[index + 3], words[index + 4], words[index + 5], words[index + 6],
+               words[index + 7]);
+        index += 8;
+        printf("%08lX%08lX%08lX%08lX%08lX%08lX%08lX%08lX", words[index], words[index + 1], words[index + 2], words[index + 3], words[index + 4], words[index + 5], words[index + 6],
+               words[index + 7]);
+        index += 8;
+        printf("%08lX%08lX%08lX%08lX%08lX%08lX%08lX%08lX", words[index], words[index + 1], words[index + 2], words[index + 3], words[index + 4], words[index + 5], words[index + 6],
+               words[index + 7]);
+        index += 8;
+        printf("%08lX%08lX%08lX%08lX%08lX%08lX%08lX%08lX", words[index], words[index + 1], words[index + 2], words[index + 3], words[index + 4], words[index + 5], words[index + 6],
+               words[index + 7]);
+        index += 8;
 
-        // cam_rot[1] += 0.1f;
-
-        t3d_mat4_identity(&model_matrix);
-        t3d_mat4_to_fixed(model_matrix_fp, &model_matrix);
-
-        rdpq_attach(display_get(), &depth_buffer);
-        t3d_frame_start();
-
-        t3d_viewport_attach(&viewport);
-
-        rdpq_mode_combiner(RDPQ_COMBINER_SHADE);
-        t3d_screen_clear_color(RGBA32(100, 0, 100, 0));
-        t3d_screen_clear_depth();
-
-        t3d_light_set_ambient(ambient_colour);
-        t3d_light_set_directional(0, colour_dir, &light_dir);
-        t3d_light_set_count(1);
-
-        t3d_state_set_drawflags(T3D_FLAG_SHADED | T3D_FLAG_DEPTH);
-
-        if (dpl_draw == NULL) {
-            rspq_block_begin();
-
-            t3d_matrix_set_mul(model_matrix_fp, 1, 0);
-            t3d_vert_load(vertices, 4);
-            t3d_tri_draw(0, 1, 2);
-            t3d_tri_draw(2, 3, 0);
-
-            t3d_tri_sync();
-
-            dpl_draw = rspq_block_end();
-        }
-
-        rspq_block_run(dpl_draw);
-
-        rdpq_detach_show();
+        console_render();
     }
-
-    t3d_destroy();
 }
