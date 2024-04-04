8d4b07cff54cceeb66bc1b97dc92747127e1c68b
Jhynjhiruu <cub3r12@gmail.com>
2024-04-04 18:23:04 +0100

    stat

--- src/main.c
+++ src/main.c
@@ -91,11 +91,105 @@ bb_ticket_bundle_t bundle = {
     .ticket_cmd = {NULL, NULL, NULL, NULL, NULL},
 };
 
-int main(void) {
+typedef struct {
+    uint32_t type;
+    char filename[8 + 1 + 3 + 1]; // 8.3 + null terminator
+} direntry_t;
+
+typedef enum {
+    READDIR_SUCCESS = 0,
+    READDIR_NOMEM,
+    READDIR_LISTFAILED,
+} ReadDirStatus;
+
+ReadDirStatus read_dir(const char *const dir, direntry_t **out, unsigned int *count) {
+    dir_t buf;
+    int status;
+
+    *count = 0;
+    *out = malloc(1);
+    if (*out == NULL) {
+        return READDIR_NOMEM;
+    }
+
+    status = dir_findfirst(dir, &buf);
+    if (status != 0) {
+        free(*out);
+        return READDIR_LISTFAILED;
+    }
+
+    while (status == 0) {
+        direntry_t *temp = realloc(*out, sizeof(direntry_t) * (*count + 1));
+        if (temp == NULL) {
+            free(*out);
+            return READDIR_NOMEM;
+        }
+
+        *out = temp;
+
+        (*out)[*count].type = buf.d_type;
+        strncpy((*out)[*count].filename, buf.d_name, 8 + 1 + 3 + 1);
+
+        (*count)++;
+
+        status = dir_findnext(dir, &buf);
+    }
+
+    return READDIR_SUCCESS;
+}
+
+int exists(const char *const drive, const char *const filename, off_t *out) {
     struct stat stat_buf;
-    int16_t *blocks;
-    void *entrypoint;
-    void *load_addr;
+    char *name;
+    size_t num = snprintf(NULL, 0, "%s:/%s", drive, filename) + 1;
+    name = malloc(num);
+    if (name == NULL) {
+        return -1;
+    }
+
+    snprintf(name, num, "%s:/%s", drive, filename);
+
+    if (stat(name, &stat_buf)) {
+        if (errno == ENOENT) {
+            return 0;
+        } else {
+            return -1;
+        }
+    } else {
+        *out = stat_buf.st_size;
+        return 1;
+    }
+}
+
+char *name_from_cid(uint32_t cid, const char *const ext) {
+    char ext_buf[4];
+
+    char *ret = malloc(8 + 1 + 3 + 1);
+    if (ret == NULL) {
+        return NULL;
+    }
+
+    memcpy(ext_buf, ext, 3);
+    ext_buf[3] = 0;
+
+    snprintf(ret, 8 + 1 + 3 + 1, "%08lx.%-3s", cid, ext_buf);
+
+    return ret;
+}
+
+// clang-format off
+#define PANIC(msg, ...) { console_clear(); printf(msg __VA_OPT__(,) __VA_ARGS__); console_render(); while (1); }
+// clang-format on
+
+int main(void) {
+    off_t ticket_size;
+    int status;
+    FILE *ticket_file;
+    uint8_t *ticket_buf;
+    uint32_t num_tickets;
+    Ticket *tickets;
+
+    char fname_buf[100];
 
     *MI_HW_INTR_MASK = (1 << 25);
 
@@ -103,109 +197,64 @@ int main(void) {
 
     console_set_render_mode(RENDER_MANUAL);
 
-    printf("Line %d\n", __LINE__);
-    console_render();
-
     if (bbfs_init()) {
-        console_clear();
-        printf("Error initialising BBFS\n");
-        console_render();
-        while (1)
-            ;
+        PANIC("Error initialising BBFS\n");
     }
 
-    printf("Line %d\n", __LINE__);
-    console_render();
-
-    if (stat("bbfs:/00000001.app", &stat_buf)) {
-        console_clear();
-        printf("Error calling stat: %d\n", errno);
-        console_render();
-        while (1)
-            ;
+    status = exists("bbfs", "ticket.sys", &ticket_size);
+    if (status < 0) {
+        PANIC("Error checking whether ticket.sys exists: %d\n", errno);
     }
 
-    printf("Line %d\n", __LINE__);
-    console_render();
-
-    blocks = bbfs_get_file_blocks("00000001.app");
-    if (blocks == NULL) {
-        console_clear();
-        printf("Error getting file blocks\n");
-        console_render();
-        while (1)
-            ;
+    if (status == 0) {
+        PANIC("ticket.sys does not exist\n");
     }
 
-    printf("Line %d\n", __LINE__);
-    console_render();
+    ticket_buf = malloc(sizeof(uint8_t) * ticket_size);
 
-    ticket.cmd.head.size = stat_buf.st_size;
-
-    if (skc_launch_setup(&bundle, NULL, NULL)) {
-        console_clear();
-        printf("Error calling skLaunchSetup");
-        console_render();
-        while (1)
-            ;
+    ticket_file = fopen("bbfs:/ticket.sys", "r");
+    if (ticket_file == NULL) {
+        PANIC("Failed to open ticket.sys: %d\n", errno);
     }
 
-    printf("Line %d\n", __LINE__);
-    console_render();
-
-    nand_mmap_begin();
-
-    if (nand_mmap(0x10000000, blocks, NAND_MMAP_ENCRYPTED)) {
-        console_clear();
-        printf("Error mapping file blocks\n");
-        console_render();
-        while (1)
-            ;
+    if (fread(ticket_buf, sizeof(uint8_t), ticket_size, ticket_file) < 4) {
+        PANIC("Failed to read ticket.sys\n");
     }
 
-    nand_mmap_end();
+    num_tickets = *(uint32_t *)ticket_buf;
+    tickets = (Ticket *)(ticket_buf + 4);
 
-    printf("Line %d\n", __LINE__);
-    console_render();
+    for (uint32_t i = 0; i < num_tickets; i++) {
+        off_t size;
 
-    wait_ms(500);
+        char *rec = name_from_cid(tickets[i].cmd.head.cid, "rec");
+        if (rec == NULL) {
+            PANIC("Failed to format rec name\n");
+        }
 
-    entrypoint = (void *)io_read(0x10000008);
-    load_addr = entrypoint - 0x1000;
+        if (exists("bbfs", rec, &size) > 0) {
+            printf("%s\n", rec);
+        } else {
+            char *app = name_from_cid(tickets[i].cmd.head.cid, "app");
+            if (app == NULL) {
+                PANIC("Failed to format app name\n");
+            }
 
-    printf("Line %d\n", __LINE__);
-    printf("Entrypoint: %p, base addr: %p\n", entrypoint, (void *)((*((volatile uint32_t *)0xA4610504) & 0xFFFF) << 14));
-    console_render();
+            if (exists("bbfs", app, &size) > 0) {
+                printf("%s\n", app);
+            } else {
+                char *aes = name_from_cid(tickets[i].cmd.head.cid, "aes");
+                if (aes == NULL) {
+                    PANIC("Failed to format aes name\n");
+                }
 
-    wait_ms(1500);
-
-    dma_read(load_addr, 0x10000000, MIN(stat_buf.st_size, 1024 * 1024 + 0x1000));
-
-    printf("Line %d\n", __LINE__);
-    console_render();
-
-    data_cache_writeback_invalidate_all();
-    inst_cache_invalidate_all();
-
-    printf("Line %d\n", __LINE__);
-    console_render();
-
-    *(volatile uint32_t *)0x80000300 = 1;
-    *(volatile uint32_t *)0x80000308 = 0xB0000000;
-    *(volatile uint32_t *)0x80000318 = 8 * 1024 * 1024;
-
-    printf("Entrypoint: %p\n", entrypoint);
-    printf("%08lX %08lX %08lX %08lX\n", io_read(0x10000000), io_read(0x10000004), io_read(0x10000008), io_read(0x1000000C));
-    console_render();
-
-    wait_ms(1500);
-
-    if (entrypoint != NULL) {
-        printf("%08lX %08lX %08lX %08lX\n", *(uint32_t *)(entrypoint), *(uint32_t *)(entrypoint + 0x04), *(uint32_t *)(entrypoint + 0x08), *(uint32_t *)(entrypoint + 0x0C));
+                if (exists("bbfs", aes, &size) > 0) {
+                    printf("%s\n", aes);
+                }
+                free(aes);
+            }
+            free(app);
+        }
+        free(rec);
     }
-    console_render();
-
-    wait_ms(1500);
-
-    skc_launch(entrypoint);
 }
